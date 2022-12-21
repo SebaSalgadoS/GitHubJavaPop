e49330441e6f7b220390578872107f167ad35a63
Sebastian Salgado <salgadoseba89@gmail.com>
2022-12-21 10:46:48 -0300

    cree metodos GET getRepositories y getPullRequest para calls a la api

---
+++ app/src/main/java/com/example/githubjavapop/data/network/RepoApiService.kt
@@ -0,0 +1,17 @@
+package com.example.githubjavapop.data.network
+
+import com.example.githubjavapop.data.model.retrofit.PullsModel
+import com.example.githubjavapop.data.model.retrofit.RepoModel
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface RepoApiService {
+
+    @GET("search/repositories?q=language:Java&sort=stars")
+    suspend fun getRepositories():Response<List<RepoModel>>
+
+    @GET("repos/{user}/{repo}/pulls")
+    suspend fun getPullRequest(@Path("user") user: String, @Path("repo") repo: String): Response<List<PullsModel>>
+
+}
