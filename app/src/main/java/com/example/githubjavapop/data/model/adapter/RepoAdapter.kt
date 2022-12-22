e844a08479ed6348ed03e6cb3ec0f05b2a78091b
Sebastian Salgado <salgadoseba89@gmail.com>
2022-12-22 16:51:39 -0300

    agregue RepoAdapter.kt y cambie nombre de variables en UserOwner.kt

--- app/src/main/java/com/example/githubjavapop/data/model/adapter/RepoAdapter.kt
+++ app/src/main/java/com/example/githubjavapop/data/model/adapter/RepoAdapter.kt
@@ -1,33 +1,58 @@
 package com.example.githubjavapop.data.model.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.githubjavapop.R
+import com.example.githubjavapop.data.model.retrofit.Item
+import com.example.githubjavapop.data.model.retrofit.RepoModel
+import com.squareup.picasso.Picasso
 
-class RepoAdapter(): RecyclerView.Adapter<RepoAdapter.ViewHolder>() {
+class RepoAdapter(private val datos: List<Item>): RecyclerView.Adapter<RepoAdapter.ViewHolder>() {
 
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
         val txtRepoName: TextView
+        val txtRepoDescription: TextView
+        val txtRepoFork: TextView
+        val txtRepoStars: TextView
+        val txtRepoUsername: TextView
+        val imgRepoUser: ImageView
 
         init{
             txtRepoName = view.findViewById(R.id.txtRepoName)
+            txtRepoDescription = view.findViewById(R.id.txtRepoDescription)
+            txtRepoFork = view.findViewById(R.id.txtRepoFork)
+            txtRepoStars = view.findViewById(R.id.txtRepoStars)
+            txtRepoUsername = view.findViewById(R.id.txtRepoUsername)
+            imgRepoUser =view.findViewById(R.id.imgRepoUser)
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.items_repos, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val repo = datos.get(position)
+
+        holder.txtRepoName.text = repo.repoName
+        holder.txtRepoDescription.text = repo.repoDescription
+        holder.txtRepoFork.text = repo.forks.toString()
+        holder.txtRepoStars.text = repo.stars.toString()
+        holder.txtRepoUsername.text = repo.repoOwner.ownerName
+
+        Picasso.get().load(repo.repoOwner.ownerAvatar).into(holder.imgRepoUser)
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return datos.size
     }
 }
--- app/src/main/java/com/example/githubjavapop/data/model/retrofit/UserOwner.kt
+++ app/src/main/java/com/example/githubjavapop/data/model/retrofit/UserOwner.kt
@@ -3,7 +3,7 @@ package com.example.githubjavapop.data.model.retrofit
 import com.google.gson.annotations.SerializedName
 
 data class UserOwner(
-    @SerializedName("id") val pullOwnerId: Int,
-    @SerializedName("login") val pullOwnerName: String,
-    @SerializedName("avatar_url") val pullOwnerAvatar: String
+    @SerializedName("id") val ownerId: Int,
+    @SerializedName("login") val ownerName: String,
+    @SerializedName("avatar_url") val ownerAvatar: String
 )
