6231538222a177a1f263fd7fc5a88a786b57c304
Sebastian Salgado <salgadoseba89@gmail.com>
2023-01-10 14:03:01 -0300

    [Fix: inject constructor and implement coroutines ]
    implement the Inject tag fot Hilt and add coroutines in the logic the getRepository function

--- app/src/main/java/com/example/githubjavapop/domain/GetRepoUserCase.kt
+++ app/src/main/java/com/example/githubjavapop/domain/GetRepoUserCase.kt
@@ -1,20 +1,22 @@
 package com.example.githubjavapop.domain
 
-import android.widget.Toast
-import com.example.githubjavapop.data.model.retrofit.Repo
+
 import com.example.githubjavapop.data.model.retrofit.RepoModel
 import com.example.githubjavapop.data.network.GitHubApiService
 import com.example.githubjavapop.di.ApiRepoModule
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Call
+import javax.inject.Inject
 
-class GetRepoUserCase {
+class GetRepoUserCase@Inject constructor() {
 
     suspend fun getRepository(): RepoModel? {
-        val repoCallBack: GitHubApiService = ApiRepoModule.retrofit(ApiRepoModule.baseUrl()).create(GitHubApiService::class.java)
-        val callResult: Call<RepoModel> = repoCallBack.getRepositories()
-        val repoResult: RepoModel? = callResult.execute().body()
-        return repoResult
+        return withContext(Dispatchers.IO){
+            val repoCallBack: GitHubApiService = ApiRepoModule.retrofit(ApiRepoModule.baseUrl()).create(GitHubApiService::class.java)
+            val callResult: Call<RepoModel> = repoCallBack.getRepositories()
+            val repoResult: RepoModel? = callResult.execute().body()
+            repoResult
+        }
     }
 }
