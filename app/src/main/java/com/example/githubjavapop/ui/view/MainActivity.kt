96b1639e9b5d326b8968f9583858413028bae3b8
Sebastian Salgado <salgadoseba89@gmail.com>
2023-01-10 16:37:55 -0300

    [Feat: add dependency tag on MainActivity.kt ]
    Delete unnecessary comments and add dependency tag to the activity

--- app/src/main/java/com/example/githubjavapop/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/githubjavapop/ui/view/MainActivity.kt
@@ -3,14 +3,13 @@ package com.example.githubjavapop.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.githubjavapop.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // (1) usar navigation de jetpack para los fragmentos
-
 
     }
 }
