ff0fa3f30963522aaa3391cf6e3d2b0322be3ae6
Sebastian Salgado <salgadoseba89@gmail.com>
2023-01-19 14:52:41 -0300

    [Fix: Change the use of Call methods to Retrofit Response methods]

--- app/src/main/java/com/example/githubjavapop/data/network/GitHubApiService.kt
+++ app/src/main/java/com/example/githubjavapop/data/network/GitHubApiService.kt
@@ -1,19 +1,18 @@
 package com.example.githubjavapop.data.network
 
-import com.example.githubjavapop.data.model.retrofit.Item
+
 import com.example.githubjavapop.data.model.retrofit.PullsModel
 import com.example.githubjavapop.data.model.retrofit.RepoModel
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GitHubApiService {
 
-    @GET("search/repositories?q=language:Java&sort=stars")
-    suspend fun getRepositories(): Call<List<Item>>
-
     @GET("repos/{user}/{repo}/pulls")
     suspend fun getPullRequest(@Path("user") user: String, @Path("repo") repo: String): Response<List<PullsModel>>
+
+    @GET("search/repositories?q=language:Java&sort=stars")
+    suspend fun getAllRepositories(): Response<RepoModel>
 
 }
