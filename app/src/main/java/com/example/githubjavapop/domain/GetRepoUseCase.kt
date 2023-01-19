5fedaf720edc10c31e99f4fdfb80ff2fb04b636d
Sebastian Salgado <salgadoseba89@gmail.com>
2023-01-19 15:09:18 -0300

    [Fix: Rename the file from GetRepoUserCase.kt to GetRepoUseCase.kt ]

---
+++ app/src/main/java/com/example/githubjavapop/domain/GetRepoUseCase.kt
@@ -0,0 +1,49 @@
+package com.example.githubjavapop.domain
+
+
+import android.util.Log
+import com.example.githubjavapop.data.model.retrofit.RepoItems
+import com.example.githubjavapop.data.network.GitHubApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class GetRepoUseCase@Inject constructor(private val apiService: GitHubApiService) {
+
+
+    /*
+    //TODO revisar la implementacion del viewModel en esta funcion
+
+     fun loadList2(): List<RepoItems>{
+        var respuesta = emptyList<RepoItems>()
+         CoroutineScope(Dispatchers.IO).launch{
+            kotlin.runCatching {
+                apiService.getAllRepositories()
+            }.onSuccess { response ->
+                respuesta = response.body()?.items ?: emptyList()
+                Log.e("DATA SUCCCESS", response.body()?.items.toString())
+            }.onFailure {
+                Log.e("ERROR CARGAR", it.toString())
+            }
+        }
+        return respuesta
+    }
+     */
+
+    suspend fun loadList(): List<RepoItems>{
+        return withContext(Dispatchers.IO){
+            val response = apiService.getAllRepositories()
+            if (response.isSuccessful){
+                val repo = response.body()?.items ?: emptyList<RepoItems>()
+                repo
+            }else{
+                Log.e("ERROR CARGAR", response.errorBody().toString())
+                emptyList()
+            }
+        }
+    }
+
+
+
+
+}
