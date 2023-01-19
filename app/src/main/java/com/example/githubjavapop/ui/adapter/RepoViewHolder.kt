a2970e00063465a8f0919591464de9fce8885333
Sebastian Salgado <salgadoseba89@gmail.com>
2023-01-19 14:42:53 -0300

    [Fea: Create class ViewHolder for repository adapter ]
    create ViewHolder class file outside adapter with repository data

---
+++ app/src/main/java/com/example/githubjavapop/ui/adapter/RepoViewHolder.kt
@@ -0,0 +1,36 @@
+package com.example.githubjavapop.ui.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.githubjavapop.data.model.retrofit.RepoItems
+import com.example.githubjavapop.databinding.ItemsReposBinding
+import com.squareup.picasso.Picasso
+
+class RepoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val binding = ItemsReposBinding.bind(view)
+
+
+    val txtRepoName = binding.txtRepoName
+    val txtRepoDescription = binding.txtRepoDescription
+    val txtRepoFork = binding.txtRepoFork
+    val txtRepoStars = binding.txtRepoStars
+    val txtRepoUsername = binding.txtRepoUsername
+    val imgRepoUser = binding.imgRepoUser
+    val repoContent = binding.repoContent
+
+
+    fun initViewHolder(repo: RepoItems) {
+
+        txtRepoName.text = repo.repoName
+        txtRepoDescription.text = repo.repoDescription
+        txtRepoFork.text = repo.forks.toString()
+        txtRepoStars.text = repo.stars.toString()
+        txtRepoUsername.text = repo.repoOwner.ownerName
+
+        repoContent.setOnClickListener {}
+
+        Picasso.get().load(repo.repoOwner.ownerAvatar).into(imgRepoUser)
+
+    }
+}
