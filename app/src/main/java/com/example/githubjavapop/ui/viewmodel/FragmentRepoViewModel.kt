a643e3df7ad0a8c8a5f4d6e0d2ec0e4c4a5c5109
Sebastian Salgado <salgadoseba89@gmail.com>
2023-01-19 15:07:12 -0300

    [Fix: change the interactor methods in getAllRepos function]
    Change the methods of the interactor in the getAllRepos function according to the new function insde GetRepoUseCase.kt

--- app/src/main/java/com/example/githubjavapop/ui/viewmodel/FragmentRepoViewModel.kt
+++ app/src/main/java/com/example/githubjavapop/ui/viewmodel/FragmentRepoViewModel.kt
@@ -3,23 +3,21 @@ package com.example.githubjavapop.ui.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.githubjavapop.data.model.retrofit.Item
-import com.example.githubjavapop.data.model.retrofit.RepoModel
-import com.example.githubjavapop.domain.GetRepoUserCase
+import com.example.githubjavapop.data.model.retrofit.RepoItems
+import com.example.githubjavapop.domain.GetRepoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class FragmentRepoViewModel @Inject constructor(val interactor: GetRepoUserCase): ViewModel() {
+class FragmentRepoViewModel @Inject constructor(val interactor: GetRepoUseCase): ViewModel() {
 
-    // tiene que ser de tipo List<Item>
-    val repositoryModel = MutableLiveData<List<Item>>()
+    val repositoryModel = MutableLiveData<List<RepoItems>>()
 
     fun getAllRepos(){
         viewModelScope.launch{
-            repositoryModel.postValue(interactor.getRepository())
-            println("DATA ITERACTOR" + interactor.getRepository())
+            repositoryModel.postValue(interactor.loadList())
+            println("DATA ITERACTOR" + interactor.loadList())
         }
 
     }
