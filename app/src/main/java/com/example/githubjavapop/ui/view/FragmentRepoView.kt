2143a134220ba40dd7927ce587edef06d21ceafb
Sebastian Salgado <salgadoseba89@gmail.com>
2023-01-23 15:48:45 -0300

    [Feat: implement viewModel observer, add recyclerview implementation and onClick function with navigation logic ]

--- app/src/main/java/com/example/githubjavapop/ui/view/FragmentRepoView.kt
+++ app/src/main/java/com/example/githubjavapop/ui/view/FragmentRepoView.kt
@@ -1,14 +1,17 @@
 package com.example.githubjavapop.ui.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.githubjavapop.R
+import com.example.githubjavapop.data.model.retrofit.RepoItems
 import com.example.githubjavapop.databinding.FragmentRepoViewBinding
 import com.example.githubjavapop.ui.adapter.RepoAdapter
 import com.example.githubjavapop.ui.viewmodel.FragmentRepoViewModel
@@ -20,38 +23,37 @@ class FragmentRepoView : Fragment() {
 
     private val repoViewModel: FragmentRepoViewModel by viewModels()
 
-    private lateinit var binding: FragmentRepoViewBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
+    private val binding by lazy { FragmentRepoViewBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_repo_view, container, false)
-
-
-       // val recyclerView = binding.repoRecyclerView
-       // recyclerView.layoutManager = LinearLayoutManager(this)
-
-
-        repoViewModel.getAllRepos()
-
-
-        repoViewModel.repositoryModel.observe(viewLifecycleOwner , Observer{
-           // binding.repoRecyclerView.adapter = RepoAdapter(it) //recibe un List<Item>
-            println("DATA FRAGMENTO " + repoViewModel.getAllRepos())
-        })
-
-
+        return binding.root
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = binding.repoRecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        repoViewModel.getAllRepos()
+
+        repoViewModel.repositoryModel.observe(viewLifecycleOwner, Observer {
+            recyclerView.adapter = RepoAdapter(it) {repos -> onItemSelected(repos) }
+            Log.e("DATA FRAGMENTO", it.toString())
+        })
+
+    }
+
+    fun onItemSelected(repo: RepoItems){
+        Toast.makeText(context, "CLICK CLICK CLICK!!", Toast.LENGTH_SHORT).show()
+        val next_action = FragmentRepoViewDirections.actionFragmentRepoViewToFragmentPullsView()
+        findNavController().navigate(next_action)
+
+    }
 
 }
