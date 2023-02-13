eba3985aa57a3af307696b4ba27471d64fb83f29
Sebastian Salgado <salgadoseba89@gmail.com>
2023-02-13 11:43:10 -0300

    [Fix: Reformat the file and take the functions out of the onViewCreated  ]

--- app/src/main/java/com/example/githubjavapop/ui/view/FragmentPullsView.kt
+++ app/src/main/java/com/example/githubjavapop/ui/view/FragmentPullsView.kt
@@ -1,24 +1,22 @@
 package com.example.githubjavapop.ui.view
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubjavapop.data.model.ApiState
 import com.example.githubjavapop.data.model.retrofit.PullsModel
-import com.example.githubjavapop.data.service.ImageLoader
+import com.example.githubjavapop.utils.ImageLoader
 import com.example.githubjavapop.databinding.FragmentPullsViewBinding
 import com.example.githubjavapop.ui.adapter.PullsAdapter
-import com.example.githubjavapop.ui.adapter.RepoAdapter
 import com.example.githubjavapop.ui.viewmodel.FragmentPullsViewModel
 import com.example.githubjavapop.utils.ERROR_STATE
 import com.example.githubjavapop.utils.LOADING_STATE
@@ -27,15 +25,23 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class FragmentPullsView @Inject constructor(private val picasso: ImageLoader) : Fragment() {
+class FragmentPullsView : Fragment() {
 
+    @Inject
+    lateinit var picassoImage: ImageLoader
     private val binding by lazy { FragmentPullsViewBinding.inflate(layoutInflater) }
     private val args: FragmentPullsViewArgs by navArgs()
     private val pullViewModel: FragmentPullsViewModel by viewModels()
-    private val pullsAdapter by lazy { PullsAdapter(adapterManager = PullsManager()) { pulls -> onItemSelected(pulls = pulls) } }
+    private val pullsAdapter by lazy {
+        PullsAdapter(adapterManager = PullsManager()) { pulls ->
+            onItemSelected(
+                pulls = pulls
+            )
+        }
+    }
 
-    inner class PullsManager: PullsAdapter.AdapterManager{
-        override fun provideImageLoader(): ImageLoader = picasso
+    inner class PullsManager : PullsAdapter.AdapterManager {
+        override fun provideImageLoader(): ImageLoader = picassoImage
     }
 
     override fun onCreateView(
@@ -48,33 +54,57 @@ class FragmentPullsView @Inject constructor(private val picasso: ImageLoader) : 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView = binding.pullsRecyclerView
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        pullViewModel.getAllPulls(user = args.repositoriesUser,repo = args.repositoriesTitle)
-        recyclerView.adapter = pullsAdapter
+        initRecyclerView()
+        initVIewModel()
+        initObserver()
+        initToolBar()
 
-        pullViewModel.pullsRequestModel.observe(viewLifecycleOwner, Observer { state->
-            when(state){
-                is ApiState.Loading -> {
-                   binding.viewFlipper.displayedChild = LOADING_STATE
-                }
-                is ApiState.Error -> {
-                    binding.viewFlipper.displayedChild = ERROR_STATE
-                    binding.txtError.text = state.error
-                }
-                is ApiState.Success -> {
-                    binding.viewFlipper.displayedChild = SUCCESS_STATE
-                    pullsAdapter.updateAdapter(state.value)
-                }
+    }
+
+    private fun handleState(state: ApiState<List<PullsModel>, String>) {
+        when (state) {
+            is ApiState.Loading -> {
+                binding.viewFlipper.displayedChild = LOADING_STATE
             }
-        })
+            is ApiState.Error -> {
+                binding.viewFlipper.displayedChild = ERROR_STATE
+                binding.txtError.text = state.error
+            }
+            is ApiState.Success -> {
+                binding.viewFlipper.displayedChild = SUCCESS_STATE
+                pullsAdapter.updateAdapter(state.value)
+            }
+        }
+    }
+
+    private fun <T, L : MutableLiveData<T>> LifecycleOwner.observe(liveData: L, body: (T) -> Unit) =
+        liveData.observe(this, Observer(body))
+
+    private fun onItemSelected(pulls: PullsModel) {
+        val action = FragmentPullsViewDirections.actionFragmentPullsViewToFragmentPullsBrowser(
+            urlPulls = pulls.urlPull,
+            pullsTitle = pulls.pullTitle
+        )
+        findNavController().navigate(action)
+    }
+
+    private fun initToolBar() {
         binding.iconBack.setOnClickListener { findNavController().navigateUp() }
         binding.viewTittle.text = args.repositoriesTitle
     }
 
-    private fun onItemSelected(pulls: PullsModel) {
-        val action = FragmentPullsViewDirections.actionFragmentPullsViewToFragmentPullsBrowser(urlPulls = pulls.urlPull, pullsTitle = pulls.pullTitle)
-        findNavController().navigate(action)
+    private fun initObserver() {
+        observe(pullViewModel.pullsRequestModel, this@FragmentPullsView::handleState)
+    }
+
+    private fun initRecyclerView() {
+        val recyclerView = binding.pullsRecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = pullsAdapter
+    }
+
+    private fun initVIewModel() {
+        pullViewModel.getAllPulls(user = args.repositoriesUser, repo = args.repositoriesTitle)
     }
 
 }
