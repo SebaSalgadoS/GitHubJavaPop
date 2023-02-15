15c90b2f793a690f10e4f7df89ab7c1154e9d335
Sebastian Salgado <salgadoseba89@gmail.com>
2023-02-15 10:28:13 -0300

    [Feat: change the onclick event to the adaptermanager function inside the cardview ]

--- app/src/main/java/com/example/githubjavapop/ui/adapter/PullsViewHolder.kt
+++ app/src/main/java/com/example/githubjavapop/ui/adapter/PullsViewHolder.kt
@@ -4,25 +4,25 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.githubjavapop.data.model.retrofit.PullsModel
 import com.example.githubjavapop.databinding.ItemsPullsOpenBinding
-import com.squareup.picasso.Picasso
+import java.util.*
 
 class PullsViewHolder(view: View, private val adapterManager: PullsAdapter.AdapterManager) :
     RecyclerView.ViewHolder(view) {
 
     private val binding = ItemsPullsOpenBinding.bind(view)
 
-    fun initViewHolder(pulls: PullsModel, onClickListener: (PullsModel) -> Unit) = with(binding) {
+    fun initViewHolder(pulls: PullsModel) = with(binding) {
 
         txtTittlePull.text = pulls.pullTitle
         txtBodyPull.text = pulls.pullBody
         txtDatePull.text = pulls.pullDate
         txtPullUsername.text = pulls.pullOwner.ownerName
-        txtPullStatus.text = pulls.pullStatus.toUpperCase()
+        txtPullStatus.text = pulls.pullStatus.uppercase(Locale.getDefault())
 
         adapterManager.provideImageLoader().loadPicasso(pulls.pullOwner.ownerAvatar, imgPullUser)
 
         pullsContent.setOnClickListener {
-            onClickListener(pulls)
+            adapterManager.onClickListener(pulls)
         }
 
     }
