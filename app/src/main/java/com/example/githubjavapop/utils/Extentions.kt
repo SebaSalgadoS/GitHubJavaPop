6e3bbb8f36c449286de59b51b26d69653fcb0e51
Sebastian Salgado <salgadoseba89@gmail.com>
2023-03-03 11:33:02 -0300

    [Feat: add generic functions for use case ]

--- app/src/main/java/com/example/githubjavapop/utils/Extentions.kt
+++ app/src/main/java/com/example/githubjavapop/utils/Extentions.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.githubjavapop.data.model.ApiState
+import com.example.githubjavapop.data.model.retrofit.RepoItems
+import com.example.githubjavapop.data.model.retrofit.RepoModel
+import retrofit2.Response
 
 
 fun ImageView.setBackAction() {
@@ -18,7 +21,28 @@ fun SwipeRefreshLayout.onRefreshList(vModel: () -> Unit) {
     }
 }
 
-fun <T> MutableLiveData<ApiState<List<T>, String>>.clearList(){
+fun <T> MutableLiveData<ApiState<List<T>, String>>.clearList() {
     val list: ApiState<List<T>, String> = ApiState.Success(emptyList())
     this.value = list
+}
+
+fun Response<RepoModel>.toRepoMapperResponse(): Response<List<RepoItems>> {
+    return if (this.isSuccessful) {
+        Response.success(this.body()?.items, this.raw())
+    } else {
+        Response.error(this.errorBody(), this.raw())
+    }
+}
+
+fun <T> Response<List<T>>.apiResponse(): ApiState<List<T>, String> {
+    return if (this.isSuccessful) {
+        val body = this.body() ?: emptyList()
+        return if (body.isEmpty()) {
+            ApiState.Error(LIST_EMPTY)
+        } else {
+            ApiState.Success(body)
+        }
+    } else {
+        ApiState.Error(NO_DATA)
+    }
 }
