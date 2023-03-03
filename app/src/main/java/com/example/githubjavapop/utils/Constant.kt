c06ecf775e59e3387a79918d742d5e3139078823
Sebastian Salgado <salgadoseba89@gmail.com>
2023-03-03 11:31:38 -0300

    [Feat: add new const val for error message ]

--- app/src/main/java/com/example/githubjavapop/utils/Constant.kt
+++ app/src/main/java/com/example/githubjavapop/utils/Constant.kt
@@ -1,9 +1,12 @@
 package com.example.githubjavapop.utils
 
-const val  LOADING_STATE = 0
-const val  SUCCESS_STATE = 1
-const val  ERROR_STATE = 2
+const val LOADING_STATE = 0
+const val SUCCESS_STATE = 1
+const val ERROR_STATE = 2
 const val REPOADAPTER_MOST_FORKED = 100
 const val REPOADAPTER_NORMAL = 200
 const val PULLADAPTER_OPEN = 100
 const val PULLADAPTER_CLOSE = 200
+
+const val LIST_EMPTY = "List Empty"
+const val NO_DATA = "No Data Found"
